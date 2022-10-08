d7fed8d7d34c8d161b2f36bbed3178d0afb77468
Tayfur Erkenci <terkenci@gmail.com>
2022-10-08 21:26:44 +0300

    implemented rtoolkit async thunk

--- reduxtoolkit-demo/index.js
+++ reduxtoolkit-demo/index.js
@@ -3,8 +3,12 @@ const cakeActions = require('./features/cake/cakeSlice').cakeActions;
 const icecreamActions =
   require('./features/icecream/icecreamSlice').icecreamActions;
 
+const fetchUsers = require('./features/user/userSlice').fetchUsers;
+
 console.log('initial state ', store.getState());
 const unsubscribe = store.subscribe(() => {});
+
+store.dispatch(fetchUsers());
 
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
