4eb63717f85b82c4c2c766f6e9fd7edfab1d03ea
Tayfur Erkenci <terkenci@gmail.com>
2022-10-08 20:03:36 +0300

    implemented rtoolkit extra reducers

--- reduxtoolkit-demo/features/icecream/icecreamSlice.js
+++ reduxtoolkit-demo/features/icecream/icecreamSlice.js
@@ -1,3 +1,5 @@
+const { cakeActions } = require('../cake/cakeSlice');
+
 const createSlice = require('@reduxjs/toolkit').createSlice;
 
 const initialState = {
@@ -14,6 +16,17 @@ const icecreamSlice = createSlice({
     restocked: (state, action) => {
       state.numOfIcecreams += action.payload || 1;
     },
+  },
+  // not preferred aproach
+  // extraReducers: {
+  //   ['cake/ordered']: (state, action) => {
+  //     state.numOfIcecreams -= action.payload || 1;
+  //   },
+  // },
+  extraReducers: (builder) => {
+    builder.addCase(cakeActions.ordered, (state, action) => {
+      state.numOfIcecreams -= action.payload || 1;
+    });
   },
 });
 
