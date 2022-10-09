af064162a78c59597ad7af1ec4d7ed4c0f24e3c1
Tayfur Erkenci <terkenci@gmail.com>
2022-10-09 15:47:08 +0300

    implemented react-redux

--- react-redux-demo/src/features/icecream/IcecreamView.jsx
+++ react-redux-demo/src/features/icecream/IcecreamView.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { ordered, restocked } from './icecreamSlice';
+
 
 export const IcecreamView = () => {
+  const [value, setValue] = useState();
+  const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams);
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <h2>Number of Icecream - </h2>
-      <button>Order Icecream</button>
-      <button>Restock Icecreams</button>
+      <h2>Number of Icecream - {numOfIcecreams}</h2>
+      <button onClick={() => dispatch(ordered())}>Order Icecream</button>
+      <input type='number' value={value} onChange={e => setValue(parseInt(e.target.value))} />
+      <button onClick={() => dispatch(restocked(value))}>Restock Icecreams</button>
     </div>
   )
 }
